6989a6327141967da4c85c67822bb0359eac4958
Quang Ha <quangha2611@gmail.com>
2020-12-26 22:20:17 +0700

    fix readme

---
+++ storage/framework/views/2f0c5c683b232510f9c28076975f26231db04e08.php
@@ -0,0 +1,21 @@
+<ul>
+  abc
+  <?php $__currentLoopData = $items; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $menu_item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <li>
+      <a class="text-center" href="<?php echo e($menu_item->url); ?>"><?php echo e($menu_item->title); ?>
+
+      </a>
+      <?php
+        $submenu = $menu_item->children;
+      ?>
+
+      <?php if(isset($submenu)): ?>
+        <ul class="menu">
+          <?php $__currentLoopData = $submenu; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <li><a href="<?php echo e($item->url); ?>"><?php echo e($item->title); ?> </a></li>
+          <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+      <?php endif; ?>
+    </li>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</ul><?php /**PATH /home/quangha/Desktop/voyager/voyager_test/resources/views/my_menu.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/5323fc639d175fa494af53a37560eed5ae35a809.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha512-6MXa8B6uaO18Hid6blRMetEIoPqHf7Ux1tnyIQdpt9qI5OACx7C+O3IVTr98vwGnlcg0LOLa02i9Y1HpVhlfiw==" crossorigin="anonymous" />
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php echo menu('ctv', 'my_menu'); ?>
+
+    </body>
+</html>
+<?php /**PATH /home/quangha/Desktop/voyager/voyager_test/resources/views/welcome.blade.php ENDPATH**/ ?>
